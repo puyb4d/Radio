656e51aca5b0a240f2630533efdff7f4cc59c118
Stéphane Puybareau <stephane.puybareau@4d.com>
2014-10-08 17:47:30 +0200

    first commit

---
+++ designer.js
@@ -0,0 +1,40 @@
+﻿(function(Radio) {
+
+//    /* Default width and height of your widget */
+//    Radio.setWidth('200');
+//    Radio.setHeight('20');
+
+//    /* Define custom event for your widget */
+//    Radio.addEvent('myEvent');
+
+//    /* Customize existing properties */
+//    Radio.customizeProperty('test', {
+//        title: 'Test Static Value',
+//        description: 'Add a datasource to this property.'
+//    });
+
+//    /* Add a Label property */
+//    Radio.addLabel({
+//        'defaultValue': '',
+//        'position': 'top'
+//    });
+
+//    /* Set the Design and Styles panels */
+//    Radio.setPanelStyle({
+//        'fClass': true,
+//        'text': true,
+//        'background': true,
+//        'border': true,
+//        'sizePosition': true,
+//        'label': true,
+//        'disabled': ['border-radius']
+//    });
+
+//    /* Override widget's initialization */
+//    Radio.prototype.init = function() {
+//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
+//    }
+
+});
+
+// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html
---
+++ widget.js
@@ -0,0 +1,69 @@
+﻿WAF.define('Radio', ['waf-core/widget', 'DropDown'], function(widget, DropDown) {
+    "use strict";
+
+    var Radio = widget.create('Radio', DropDown, {
+        tagName: 'ul',
+        render: function(elements) {
+            var s = '';
+            s += elements.map(function(element, index) {
+                return '<li>' + 
+                    '<input type="radio" name="' + this.id + '" id="' + this.id + '-' + index + '" value="' + element.value + '">' +
+                    '<label for="' + this.id + '-' + index + '">' + element.label + '</label>' +
+                    '</li>';
+            }.bind(this)).join('');
+            this.node.innerHTML = s;
+            this._valueChangeHandler();
+        },
+        _valueChangeHandler: function() {
+            var value = this.value();
+            var opt = $('[value=' + (value || '') + ']', this.node).get(0);
+            if(opt) {
+                opt.checked = true;
+            } else {
+                this.fire('errorNotFound');
+            }
+        },
+        getSelectedIndex: function() {
+            return $('input:checked', this.node).parent().index();
+        },
+        init: function() {
+            DropDown.prototype.init.call(this);
+            var that = this;
+            $(this.node).on('mousedown', function(event) {
+                var node = event.target;
+                if(node.tagName === 'LABEL') {
+                    node = document.getElementById(node.for);
+                }
+                if(node.tagName !== 'INPUT') {
+                    return;
+                }
+                var $node = $(node);
+                if(node.checked && that.allowEmpty()) {
+                    var uncheck = function(){
+                        setTimeout(function() {
+                            node.checked = false;
+                            that._setValueByPosition(-1);
+                        }, 0);
+                    };
+                    var unbind = function(){
+                        $node.unbind('mouseup', up);
+                    };
+                    var up = function(){
+                        uncheck();
+                        unbind();
+                    };
+                    $node.bind('mouseup', up);
+                    $node.one('mouseout', unbind);
+                } else {
+                    node.checked = true;
+                    var position = that.getSelectedIndex();
+                    that._setValueByPosition(position);
+                }
+
+            });
+        }
+    });
+
+    return Radio;
+
+});
